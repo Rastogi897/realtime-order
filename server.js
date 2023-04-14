7ee2fe206dd8c4e88ac23d712fca8ed2e59dc3af
Tushar Rastogi <tusharrstg@gmail.com>
2023-04-15 00:28:50 +0530

    laravel & tailwind setup

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 const ejs = require("ejs")
 const expressLayout = require("express-ejs-layouts")
 const path = require("path")
-
 const PORT = process.env.PORT || 3300
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
